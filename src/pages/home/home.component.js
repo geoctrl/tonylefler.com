fcac3f00144d33c7c479556190d3921ee6a19025
tonylefler <tonylefler@gmail.com>
2017-11-02 09:07:05 -0600

    under construction message

--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -6,5 +6,7 @@ export const HomePage = Vue.component('homePage', {
   template:
 `<div>
     <title-header></title-header>
+    
+    <p class="text-center">Site is under Construction - Come back soon.</p>
 </div>`
 });
