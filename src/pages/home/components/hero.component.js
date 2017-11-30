b08665328129ffae3bce6f7a7c73ca304ecc63ab
tonylefler <tonylefler@gmail.com>
2017-11-30 11:53:49 -0700

    got threejs rendering cool line things

---
+++ src/pages/home/components/hero.component.js
@@ -0,0 +1,9 @@
+import Vue from 'vue';
+
+export const HeroComponent = Vue.component('hero', {
+  template:
+`<div class="hero">
+    <div class="hero__name">Tony Lefler</div>
+    <div class="hero__title">Front End Developer</div>
+</div>`
+});
--- src/pages/home/home.component.js
+++ src/pages/home/home.component.js
@@ -1,12 +1,91 @@
 import Vue from 'vue';
+import { Scene, PerspectiveCamera, WebGLRenderer, LineBasicMaterial, Vector3, Geometry, Line, FogExp2, Euler } from 'three';
 
-import { TitleHeaderComponent } from './components/title-header.component';
+import { HeroComponent } from './components/hero.component';
+const colors = ['#E0D4BC', '#05C4B4', '#FF9D00', '#AF5CC6', '#45D2EF', '#CC3F3F'];
 
 export const HomePage = Vue.component('homePage', {
   template:
 `<div>
-    <title-header></title-header>
-    
-    <p class="text-center">Site is under Construction - Come back soon.</p>
-</div>`
+    <!--<hero></hero>-->
+    <!--<p class="text-center">Site is under Construction - Come back soon.</p>-->
+</div>`,
+
+  mounted() {
+
+    let scene = new Scene();
+    scene.fog = new FogExp2( 0xffffff, 0.006 );
+
+
+    let camera = new PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 500);
+    camera.position.set(0, 0, 100);
+    // camera.lookAt(new Vector3(0, 0, 0));
+
+    let renderer = new WebGLRenderer({
+      alpha: true,
+      antialias: true
+    });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    generateVertices(scene);
+
+    function animate() {
+      requestAnimationFrame( animate );
+      // camera.position.y = camera.position.y-.05;
+      // camera.position.z = camera.position.z-.05;
+      camera.rotateZ(.0003);
+      renderer.render( scene, camera );
+    }
+    animate();
+  }
 });
+
+
+function generateVertices(scene) {
+  let range = 100;
+  let quads = [
+    { x: -range, y: -range },
+    { x: 0, y: -range },
+    { x: -range, y: 0 },
+    { x: range, y: range },
+  ];
+  quads = shuffle(quads);
+  let lines = Array.from({length: randomNumber(50, 100)}, () => 0);
+
+  lines.forEach(() => {
+    let material = new LineBasicMaterial({ color: colors[randomNumber(0, colors.length-1)] });
+    let geometry = new Geometry();
+    quads.forEach(q => {
+      geometry.vertices.push(new Vector3(
+          randomNumber(q.x, q.x + range),
+          randomNumber(q.y, q.y + range),
+          randomNumber(-200, 200)
+      ));
+    });
+    scene.add(new Line(geometry, material));
+  });
+
+  function randomNumber(min, max) {
+    let testMin = min+range;
+    let testMax = max+range;
+    let test = Math.floor(Math.random()*(testMax-testMin+1)+testMin);
+    return test - range;
+  }
+}
+
+function shuffle(array) {
+  let copy = [], n = array.length, i;
+
+  // While there remain elements to shuffle…
+  while (n) {
+
+    // Pick a remaining element…
+    i = Math.floor(Math.random() * n--);
+
+    // And move it to the new array.
+    copy.push(array.splice(i, 1)[0]);
+  }
+
+  return copy;
+}
